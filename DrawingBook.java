d7dd5ee160896b2c8cd94a37572c58e4527d8790
Arun-nmcc <r.arun07042002@gmail.com>
2022-07-16 17:40:07 +0530

    hackerank

---
+++ DrawingBook.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DrawingBook {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n= scan.nextInt()/2;
+        int p= scan.nextInt()/2;
+        int back=n-p;
+        System.out.println(Math.min(back,p));
+
+
+
+
+    }
+
+}
